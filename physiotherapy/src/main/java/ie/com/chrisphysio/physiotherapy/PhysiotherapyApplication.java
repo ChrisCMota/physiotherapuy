d32260fe5951098655df2d5f4143c05bb6f1af6b
ChrisCMota <100359859+ChrisCMota@users.noreply.github.com>
2024-10-18 14:16:10 +0100

    Project initialized
    First commit

---
+++ physiotherapy/src/main/java/ie/com/chrisphysio/physiotherapy/PhysiotherapyApplication.java
@@ -0,0 +1,13 @@
+package ie.com.chrisphysio.physiotherapy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysiotherapyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysiotherapyApplication.class, args);
+	}
+
+}
---
+++ physiotherapy/src/test/java/ie/com/chrisphysio/physiotherapy/PhysiotherapyApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.com.chrisphysio.physiotherapy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysiotherapyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
